ac8902d84c1f8fc77e20ef10e227da25b28a9571
Felipe Morais <morais.alves.felipe@gmail.com>
2024-05-31 22:18:17 -0300

    31/05/2024 atualizando o contato e form

--- pages/contato.php
+++ pages/contato.php
@@ -1,82 +1,51 @@
 <div class="container">
-<main class="main">
-    <div>
-        <br>
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    <main class="main">
+        <div>
+            <br>
+            <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 
-        <div class="container"> 
+                <div class="container"> 
     
-            <form action="">
+                    <form action="">
         
-            <div class="row">
-            <div class="col-25">
-            <label for="fname">First Name</label>
-            </div>
-            <div class="col-75">
-            <input type="text" id="fname" name="firstname" placeholder="Your name..">
-            </div>
-            </div>
+                        <div class="row">
+                            <div class="col-25">
+                                <label for="fname">Nome</label>
+                            </div>
+                            <div class="col-75">
+                                <input type="text" id="fname" name="firstname" placeholder="Seu nome">
+                            </div>
+                        </div>
         
-            <div class="row">
-            <div class="col-25">
-            <label for="lname">Last Name</label>
-            </div>
-            <div class="col-75">
-            <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-            </div>
-            </div>    
-        
-            <div class="row">
-            <div class="col-25">
-            <label for="email">Email</label>
-            </div>
-            <div class="col-75">
-            <input type="email" id="email" name="email" placeholder="Your email..">
-            </div>
-            </div>
-        
-            <div class="row">
-                <div class="col-25">
-                <label for="country">Country</label>
-            </div>
-            <div class="col-75">
-                <select id="country" name="country">
-                    <option value="belgium">Belgium</option>
-                    <option value="canada">Canada</option>
-                    <option value="france">France</option>
-                    <option value="germany">Germany</option>
-                    <option value="portugal">Portugal</option>
-                    <option value="spain">Spain</option>
-                </select>
-            </div>
-        </div>
-        
-    <div class="row">
-        <div class="col-25">
-            <label for="subject">Your message..</label>
-        </div>
-        <div class="col-75">
-            <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-        </div>
-    </div>
-        
-    <div class="row">
-        <button>Submit</button>
-    </div>
-        
-    </form>
-</div>
+                        <div class="row">
+                            <div class="col-25">
+                                <label for="email">E-mail</label>
+                            </div>
+                            <div class="col-75">
+                                <input type="email" id="email" name="email" placeholder="Seu melhor e-mail">
+                            </div>
+                        </div>
 
+                        <div class="row">
+                            <div class="col-25">
+                                <label for="subject">Mensagem</label>
+                            </div>
+                            <div class="col-75">
+                                <textarea id="subject" name="subject" placeholder="Escreva aqui..." style="height:200px"></textarea>
+                            </div>
+                        </div>
+        
+                        <div class="row">
+                            <button>Enviar</button>
+                        </div>
+                    </form>
+                </div>
+        </div>
 
-                </div>
-                
-                <div class="google-maps">
-                        <br>
-                                <h1><i class="fa-solid fa-location-dot"> &nbsp; onde estamos:</i></h1>
-                        <br>
-                        
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1821.761011660139!2d-52.382064920353294!3d-24.047914822269057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ed754071f8391b%3A0x6c679332dde3e683!2sCentro%20Universit%C3%A1rio%20Grupo%20Integrado%2C%20Centro!5e0!3m2!1spt-BR!2sbr!4v1717200528363!5m2!1spt-BR!2sbr" class="map-iframe" width="1200px" height="600px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                
-                </div>
-        </main>
+        <div class="google-maps">
+            <br>
+                <h1><i class="fa-solid fa-location-dot"> &nbsp; onde estamos:</i></h1><br>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1821.761011660139!2d-52.382064920353294!3d-24.047914822269057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ed754071f8391b%3A0x6c679332dde3e683!2sCentro%20Universit%C3%A1rio%20Grupo%20Integrado%2C%20Centro!5e0!3m2!1spt-BR!2sbr!4v1717200528363!5m2!1spt-BR!2sbr" class="map-iframe" width="1200px" height="600px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </main>
 </div>
--- pages/jogos.php
+++ pages/jogos.php
@@ -55,7 +55,7 @@
                 </div>
                 <div class="col2">
                     
-                    <img src="images/mvgaming.jpg" class="col-image" title="MV Gaming" alt="MV Gaming">
+                    <img src="images/mvgaming.jpg" class="col-image" title="Galactic Annihilation" alt="Galactic Annihilation">
                     <br>
                     <h2 class="title2">Galactic Annihilation</h2>
                     <div class="desc">
