d44f35293bef901ef02e82c512a7b0a3370f1d45
Felipe Morais <morais.alves.felipe@gmail.com>
2024-05-31 23:09:32 -0300

    31/05/2024 atualizando contato.php

--- pages/contato.php
+++ pages/contato.php
@@ -1,7 +1,8 @@
 <div class="container">
     <main class="main">
         <div>
-            <br>
+            
+            <br><h1><i class="fa-solid fa-envelope">&nbsp; Entre em contato:</i></h1>
             <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 
                 <div class="container"> 
