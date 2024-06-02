972f504adc18d582fe6396a52aa0a2b121318578
Felipe Morais <morais.alves.felipe@gmail.com>
2024-06-01 21:59:01 -0300

    01/06/2024 atualizando estilos

--- pages/404.php
+++ pages/404.php
@@ -1,7 +1,7 @@
-<div class="container" >
+<div class="container">
     <main class="main">
         <div class="banner">
-        <a href="index.php?page=home" class="banner-link">
+            <a href="index.php?page=home" class="banner-link">
                 <img src="images/404a.jpg" class="banner-image" title="Error 404" alt="Error 404">
             </a>
         </div>
